1e157d07aa55ea266c4ae9b20b24eaa0499234cf
Abhishek Patel <65705442+itsAbhishekpatel@users.noreply.github.com>
2020-09-05 09:55:35 +0530

    Add files via upload

---
+++ fork_Exec/2_SPDC.c
@@ -0,0 +1,26 @@
+  //  By Abhishek Patel
+  //  1.same program same code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	int a;
+	a = fork();
+	if (a < 0)
+	{
+		printf("child process could not be created");
+		exit(-1);
+	}
+	else if (a == 0)
+	{
+		printf("Child process : My ID is %d\n", getpid());
+	}
+	else
+	{
+		printf("Parent process : My ID is %d\n", getpid());
+	}
+	return 0;
+}
---
+++ fork_Exec/3_DP.c
@@ -0,0 +1,25 @@
+  //  By Abhishek Patel
+  //  1.same program same code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+	int a;
+	a = fork();
+	if (a < 0)
+	{
+		printf("child process could not be created");
+		exit(-1);
+	}
+	else if (a == 0)
+	{
+		execl("/bin/ls", "ls", NULL);
+	}
+	else
+	{
+		printf("Parent process : My ID is %d\n", getpid());
+	}
+	return 0;
+}
